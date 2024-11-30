8326c57b71b74729bd09c9b2ce2f770ae1ff5a69
Seva <68725282+sevcator@users.noreply.github.com>
2024-12-01 00:29:37 +0700

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-SQLITE3="$MODPATH/sqlite/$ARCH32/sqlite3"
-DIALER_PACKAGE="com.google.android.dialer"
-PHENOTYPE_PATH="/data/data/com.google.android.gms/databases/phenotype.db"
-CALLRECORDINGPROMPT="/data/data/$DIALER_PACKAGE/files/callrecordingprompt"
-
-"$SQLITE3" "$PHENOTYPE_PATH" "DELETE FROM FlagOverrides WHERE packageName = '$DIALER_PACKAGE'"
-rm -rf "$CALLRECORDINGPROMPT"
